dac7cad072e20401be135befa3aa1a208886bbaa
elisha_b <elisha.b@cestasoft.co.za>
2022-01-30 23:38:16 +0200

    first commit

---
+++ pages/calculator.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+import DropList from "../components/DropList";
+
+const DappWithContract = () => {
+
+  const { address } = useWeb3();
+
+  return (
+    < >
+      <div>
+ 
+      </div>
+    </>
+  );
+};
+
+export default DappWithContract;
